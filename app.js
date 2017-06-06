580c03346ceb3d6272695b3cf664547c81152907
Adrian Flak <laserlens@gmail.com>
2017-06-06 14:00:59 -0500

    added notes on Execution Content

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var a = 'Hellow world';
+//notes:  if no code on this page javascript still has code as Execution Context
+//notes:  Execution Context creates the global object 'this'
 
-function b(){
-    
+//notes:  the variable and the funtion will be found inside the gobal ogject
+
+var a = 'Hellow world'; //found inside global oject
+
+function b(){//found inside global oject 
+
 };
